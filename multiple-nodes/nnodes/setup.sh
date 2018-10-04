b43b09ae1ea6808dc798a66e3aed85b6a8df7ebe
tonybka <tonybka@gmail.com>
2018-10-05 04:24:46 +0700

    Gen keys from docker container

--- multiple-nodes/nnodes/setup.sh
+++ multiple-nodes/nnodes/setup.sh
@@ -155,8 +155,7 @@ do
     cp static-nodes.json $qd/dd/static-nodes.json
 
     # Generate Quorum-related keys (used by Constellation)
-    docker run -u $uid:$gid -v $pwd/$qd:/qdata $image /usr/local/bin/constellation-node --generatekeys=tm < /dev/null > /dev/null
-    docker run -u $uid:$gid -v $pwd/$qd:/qdata $image /usr/local/bin/constellation-node --generatekeys=tma < /dev/null > /dev/null
+    docker run -u $uid:$gid -v $pwd/$qd:/qdata $image bash -c "constellation-node --generatekeys=tm && constellation-node --generatekeys=tma < /dev/null > /dev/null"
     echo 'Node '$n' public key: '`cat $qd/keys/tm.pub`
 
     cp template/start-node.sh $qd/start-node.sh
