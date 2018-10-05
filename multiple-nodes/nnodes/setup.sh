673c24d8a7835ca5980ce70c989008247850759f
tonybka <tonybka@gmail.com>
2018-10-05 12:05:31 +0700

    Change the generating encode hash method

--- multiple-nodes/nnodes/setup.sh
+++ multiple-nodes/nnodes/setup.sh
@@ -69,11 +69,13 @@ do
     qd=qdata_$n
 
     # Generate the node's Enode and key
-    enode=`docker run -u $uid:$gid -v $pwd/$qd:/qdata $image /usr/local/bin/bootnode -genkey /qdata/dd/nodekey -writeaddress`
+    enode=`docker run -u $uid:$gid -v $pwd/$qd:/qdata $image /usr/local/bin/bootnode -genkey /qdata/dd/nodekey -writeaddress < /dev/null > /dev/null`
+    nodekeyContent = `docker run -u $uid:$gid -v $pwd/$qd:/qdata $image cat /qdata/dd/nodekey`
+    enodeHash = `docker run -u $uid:$gid -v $pwd/$qd:/qdata $image /usr/local/bin/bootnode -nodekeyhex $nodekeyContent -writeaddress < /dev/null > /dev/null`
 
     # Add the enode to static-nodes.json
     sep=`[[ $n < $nnodes ]] && echo ","`
-    echo '  "enode://'$enode'@'$ip':30303?discport=0"'$sep >> static-nodes.json
+    echo '  "enode://'$enodeHash'@'$ip':30303?discport=0"'$sep >> static-nodes.json
 
     let n++
 done
